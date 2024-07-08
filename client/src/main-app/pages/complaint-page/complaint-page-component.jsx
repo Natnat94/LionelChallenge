e6117a58bc07832a58ba76767d97a0171f392c98
liortest2 <lior8813@gmail.com>
2024-07-08 13:56:13 +0200

    Adjust to mobile

--- client/src/main-app/pages/complaint-page/complaint-page-component.jsx
+++ client/src/main-app/pages/complaint-page/complaint-page-component.jsx
@@ -1,20 +1,27 @@
 import TextFieldComponent from "system/text-field/text-field-component";
 import UploadPhotoButton from "./upload-photo/upload-photo-component";
+import LocationMap from "./map/map-component";
 import "./complaint-page-component.scss";
 
 export default function ComplaintPageComponent() {
   return (
     <div className="complaint-page-component">
-      <div className="complaint-header">Tell us what bothers you</div>
-      <div className="complaint-description">
-        <TextFieldComponent label="Description" />
-      </div>
       <div className="complaint-location">
-        <TextFieldComponent label="Address" />
+        {/* <TextFieldComponent label="Address" /> */}
+        <LocationMap />
       </div>
-      <div className="complaint-upload-photo">
-        {/* <ButtonComponent text="Upload photo" /> */}
-        <UploadPhotoButton />
+      <div className="complaint-form">
+        <div className="complaint-form-content">
+          <div className="complaint-header">Tell us what bothers you</div>
+          <div className="complaint-description">
+            <TextFieldComponent label="Description" />
+          </div>
+
+          <div className="complaint-upload-photo">
+            {/* <ButtonComponent text="Upload photo" /> */}
+            <UploadPhotoButton />
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ client/src/main-app/pages/complaint-page/map/map-component.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix the default icon issue with Leaflet and Webpack
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
+const LocationMap = () => {
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (error) => {
+          setError(error.message);
+        }
+      );
+    } else {
+      setError("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  return (
+    <div>
+      {location.latitude && location.longitude ? (
+        <MapContainer
+          center={[location.latitude, location.longitude]}
+          zoom={13}
+          style={{ height: "80px", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[location.latitude, location.longitude]}>
+            <Popup>You are here!</Popup>
+          </Marker>
+        </MapContainer>
+      ) : (
+        <p>{error ? `Error: ${error}` : "Getting location..."}</p>
+      )}
+    </div>
+  );
+};
+
+export default LocationMap;

7c6426e15befc926266e9fda2f5d42f1e278aacd
liortest2 <lior8813@gmail.com>
2024-07-08 11:35:45 +0200

    Add button

--- client/src/main-app/pages/welcome-page/welcome-page-component.jsx
+++ client/src/main-app/pages/welcome-page/welcome-page-component.jsx
@@ -1,9 +1,13 @@
+import ButtonComponent from "system/button/button-component";
 import "./welcome-page-component.scss";
 
 export default function WelcomePageComponent() {
   return (
     <div className="welcome-page-component">
       <div className="welcome-message">Welcome to our reporting system</div>
+      <div className="button-wrapper">
+        <ButtonComponent text="Report an issue" />
+      </div>
     </div>
   );
 }

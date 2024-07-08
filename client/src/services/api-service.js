847c143c81991aa61aeb3af8b104df7a9d3e7de5
liortest2 <lior8813@gmail.com>
2024-07-08 15:20:14 +0200

    remove comment

--- client/src/services/api-service.js
+++ client/src/services/api-service.js
@@ -1,8 +1,6 @@
 const HOST = "https://9ac5-92-174-89-249.ngrok-free.app";
 // const HOST = "http://localhost:8000";
 
-// const HOST = REMOTE_HOST;
-
 export const get = async (path) => {
   const url = getUrl(path);
   return await fetch(url);

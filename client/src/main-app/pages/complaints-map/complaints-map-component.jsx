cec89abda072afae1ed516cede575c05588ae710
liortest2 <lior8813@gmail.com>
2024-07-08 18:16:54 +0200

    Add action to the button

--- client/src/main-app/pages/complaints-map/complaints-map-component.jsx
+++ client/src/main-app/pages/complaints-map/complaints-map-component.jsx
@@ -3,7 +3,7 @@ import MapComponent from "../complaint-page/map/map-component";
 import ButtonComponent from "system/button/button-component";
 import "./complaints-map-component.scss";
 
-export default function ComplaintsMapComponent() {
+export default function ComplaintsMapComponent({ reportAComplaintPage }) {
   const [location, setLocation] = useState({ latitude: null, longitude: null });
 
   return (
@@ -16,7 +16,11 @@ export default function ComplaintsMapComponent() {
         />
       </div>
       <div className="overlay" />
-      <ButtonComponent text="Report an issue" className={"report-button"} />
+      <ButtonComponent
+        text="Report an issue"
+        className={"report-button"}
+        onClick={reportAComplaintPage}
+      />
     </div>
   );
 }

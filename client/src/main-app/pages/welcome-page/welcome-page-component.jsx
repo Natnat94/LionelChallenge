d149917636b886925cd1b271effeeaa2b69d6646
liortest2 <lior8813@gmail.com>
2024-07-08 16:50:28 +0200

    Add description

--- client/src/main-app/pages/welcome-page/welcome-page-component.jsx
+++ client/src/main-app/pages/welcome-page/welcome-page-component.jsx
@@ -4,7 +4,14 @@ import "./welcome-page-component.scss";
 export default function WelcomePageComponent({ reportAComplaint }) {
   return (
     <div className="welcome-page-component">
-      <div className="welcome-message">Welcome to our reporting system</div>
+      <div className="app-name">CommunityAccess</div>
+      <div className="app-description">
+        Welcome to our application dedicated to improving accessibility for
+        people with disabilities. This platform allows users to easily report
+        PMR (People with Reduced Mobility) accessibility issues that they
+        encounter on a daily basis, such as the absence of access ramps, holes
+        in the sidewalks, or any other obstacle.
+      </div>
       <div className="button-wrapper">
         <ButtonComponent text="Report an issue" onClick={reportAComplaint} />
       </div>

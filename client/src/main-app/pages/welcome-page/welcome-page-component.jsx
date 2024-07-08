e0c3c03ca36d2af0722405b18fe058b3bc6a719f
liortest2 <lior8813@gmail.com>
2024-07-08 11:27:41 +0200

    welcome page

--- client/src/main-app/pages/welcome-page/welcome-page-component.jsx
+++ client/src/main-app/pages/welcome-page/welcome-page-component.jsx
@@ -1,3 +1,9 @@
+import "./welcome-page-component.scss";
+
 export default function WelcomePageComponent() {
-  return <div className="welcome-page-component"></div>;
+  return (
+    <div className="welcome-page-component">
+      <div className="welcome-message">Welcome to our reporting system</div>
+    </div>
+  );
 }

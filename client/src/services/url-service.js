284fda6781773255e27959ec244746fd18c58baa
liortest2 <lior8813@gmail.com>
2024-07-09 12:52:09 +0200

    remove unused code

--- client/src/services/url-service.js
+++ client/src/services/url-service.js
@@ -1,12 +1,8 @@
+const PICTURE_HOST = "https://6a25-92-174-89-249.ngrok-free.app";
 export const changeHost = (url) => {
   try {
     const urlObj = new URL(url);
-    urlObj.hostname = "https://1065-92-174-89-249.ngrok-free.app";
-    return (
-      "https://6a25-92-174-89-249.ngrok-free.app" +
-      urlObj.pathname +
-      urlObj.search
-    );
+    return PICTURE_HOST + urlObj.pathname + urlObj.search;
   } catch (error) {
     console.error("Invalid URL:", error);
     return null;
